4795713afccecaf436dcbb1917b51414f0ff9d82
tiwarianirudh <anti5275@colorado.edu>
2016-11-04 22:45:09 -0600

    creating source and header for profiler

---
+++ Project_3/project_3/Debug/Sources/profiler.d
@@ -0,0 +1,16 @@
+Sources/profiler.o: ../Sources/profiler.c ../Includes/profiler.h \
+ ../Includes/MKL25Z4.h ../Includes/core_cm0plus.h \
+ ../Includes/core_cmInstr.h ../Includes/core_cmFunc.h \
+ ../Includes/system_MKL25Z4.h
+
+../Includes/profiler.h:
+
+../Includes/MKL25Z4.h:
+
+../Includes/core_cm0plus.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/system_MKL25Z4.h:
---
+++ Project_3/project_3/Includes/profiler.h
@@ -0,0 +1,16 @@
+/*
+ * profiler.h
+ *
+ *  Created on: Nov 4, 2016
+ *      Author: aniru
+ */
+
+#ifndef INCLUDES_PROFILER_H_
+#define INCLUDES_PROFILER_H_
+
+void initialize_counter();
+void counter_start();
+void counter_stop();
+
+
+#endif /* INCLUDES_PROFILER_H_ */
